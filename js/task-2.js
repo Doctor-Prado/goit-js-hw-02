fe3f750e08375f21143dd2044f2d7f6038b07750
prado <b.grachov24@gmail.com>
2026-06-24 18:08:08 +0300

    Refactor task functions to uncomment and enable execution of transaction, message formatting, and spam checking

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,10 @@
-// function formatMessage(message, maxLength) {
-//     return message.length <= maxLength ? message : message.slice(maxLength + "...")
-// }
+function formatMessage(message, maxLength) {
+    return message.length <= maxLength ? message : message.slice(maxLength) + "...";
+}
 
-// console.log(formatMessage("Curabitur ligula sapien", 16));
-// console.log(formatMessage("Curabitur ligula sapien", 23));
-// console.log(formatMessage("Vestibulum facilisis purus nec", 20));
-// console.log(formatMessage("Vestibulum facilisis purus nec", 30));
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
-// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
