5d2a7cbd366fbef53f4ea83398595f0a7733b640
prado <b.grachov24@gmail.com>
2026-06-24 15:33:24 +0300

    Implement shipping cost calculation for various countries in task-4.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,10 @@
-function getShippingCost(country) {
-    switch (country) {
-        case "China":
-            return "Shipping to China will cost 100 credits";
-
-        case "Chile":
-            return "Shipping to Chile will cost 250 credits";
-
-        case "Australia":
-            return "Shipping to Australia will cost 170 credits";
-
-        case "Jamaica":
-            return "Shipping to Jamaica will cost 120 credits";
-
-        default:
-            return "Sorry, there is no delivery to your country";
-    }
-}
+// function checkForSpam(message) {
+//     return message.toLowerCase().includes("spam") || message.toLowerCase().includes("sale") ? true : false
+// }
+// console.log(checkForSpam("Latest technology news"));
+// console.log(checkForSpam("JavaScript weekly newsletter"));
+// console.log(checkForSpam("Get best sale offers now!"));
+// console.log(checkForSpam("Amazing SalE, only tonight!"));
+// console.log(checkForSpam("Trust me, this is not a spam message"));
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+// console.log(checkForSpam("[SPAM] How to earn fast money?"));
