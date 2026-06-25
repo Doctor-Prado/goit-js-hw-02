93cffb9ca7447ab6ae8aaffdce1e778c0d0df720
prado <b.grachov24@gmail.com>
2026-06-25 10:12:54 +0300

    Fix message slicing in formatMessage function and simplify checkForSpam return statement

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    return message.length <= maxLength ? message : message.slice(maxLength) + "...";
+    return message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
 }
 
 console.log(formatMessage("Curabitur ligula sapien", 16));
